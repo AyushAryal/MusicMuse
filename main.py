8ff1cad05feabfdaa58e13da1a88068e2995a599
AyushAryal <ayusharyal100@gmail.com>
2023-11-17 13:05:33 +0545

    partial: LSTM implementation

--- main.py
+++ main.py
@@ -18,8 +18,10 @@ class UltimateGuitarSongDataset(Dataset):
         ]
 
         self.data = []
-        for song in songs:
-            self.data.extend(zip(song, song[1:]))
+        for song in (song for song in songs if len(song) > 1):
+            x = torch.stack(song[:-1])
+            y = torch.stack(song[1:])
+            self.data.append((x, y))
 
     def __len__(self):
         return len(self.data)
@@ -28,26 +30,30 @@ class UltimateGuitarSongDataset(Dataset):
         return self.data[idx]
 
 
-class NeuralNetwork(nn.Module):
-    def __init__(self, dim):
+class LSTMNetwork(nn.Module):
+    def __init__(self, dim, hidden=None):
         super().__init__()
-        self.linear_relu_stack = nn.Sequential(
-            nn.Linear(dim, 32),
-            nn.ReLU(),
-            nn.Linear(32, 32),
-            nn.ReLU(),
-            nn.Linear(32, 32),
-            nn.ReLU(),
-            nn.Linear(32, dim),
-        )
+        if not hidden:
+            hidden = dim
 
-    def forward(self, x):
-        return self.linear_relu_stack(x)
+        self.dim = dim
+        self.hidden = hidden
+
+        self.lstm = nn.LSTM(self.dim, self.hidden)
+        self.hidden2out = nn.Linear(self.hidden, self.dim)
+
+    def forward(self, song: torch.Tensor):
+        seq_len = song.size(0)
+        out, _ = self.lstm(song.view(seq_len, 1, -1))
+        pred = self.hidden2out(out.view(seq_len, -1))
+        return pred
 
 
 def train_loop(dataloader, model, loss, optimizer):
     model.train()
     for batch_num, (x, y) in enumerate(dataloader):
+        x = torch.squeeze(x, 0)
+        y = torch.squeeze(y, 0)
         pred = model(x)
         cost = loss(pred, y)
         cost.backward()
@@ -61,15 +67,19 @@ def test_loop(dataloader, model, loss, wv):
     model.eval()
     test_loss, correct = 0, 0
 
+    total_len = 0
     with torch.no_grad():
         for x, y in dataloader:
+            x = torch.squeeze(x, 0)
+            y = torch.squeeze(y, 0)
             pred = model(x)
             test_loss += loss(pred, y)
-            for pred_, y_ in torch.stack((pred, y), dim=1):
+            total_len += y.size(0)
+            for pred_, y_ in zip(pred, y):
                 similar = wv.similar_by_vector(pred_.numpy(), topn=5)
                 if any(np.array_equal(wv[chord], y_) for chord, _ in similar):
                     correct += 1
-    correct = correct / len(dataloader.dataset) * 100
+    correct = correct / total_len * 100
     test_loss /= len(dataloader)
     print(f"Accuracy: {correct:>0.1f}, Average Loss: {test_loss:>8f}")
 
@@ -79,7 +89,7 @@ def main():
     dim = chord_vectors["C"].size
 
     train_, test_ = random_split(UltimateGuitarSongDataset(chord_vectors), [0.8, 0.2])
-    batch_size = 256
+    batch_size = 1
     train = DataLoader(train_, batch_size=batch_size)
     test = DataLoader(test_, batch_size=batch_size, shuffle=True)
 
@@ -92,9 +102,9 @@ def main():
     )
     print(f"Using {device} device")
 
-    model = NeuralNetwork(dim).to(device)
+    model = LSTMNetwork(dim).to(device)
     loss = nn.MSELoss()
-    optimizer = torch.optim.SGD(model.parameters(), lr=1e-3)
+    optimizer = torch.optim.SGD(model.parameters(), lr=0.1)
     epochs = 2
     for epoch in range(epochs):
         print(f"Epoch: {epoch+1}")
@@ -117,23 +127,33 @@ def test():
         else "cpu"
     )
     print(f"Using {device} device")
-    model = NeuralNetwork(dim).to(device)
+    model = LSTMNetwork(dim).to(device)
     model.load_state_dict(torch.load("./out/model.pth"))
     model.eval()
 
     with torch.no_grad():
         while True:
             try:
-                s = input("Enter chord: ").strip()
+                chord_sequence_str = input("Enter chord sequence: ").strip()
             except KeyboardInterrupt:
                 break
-            pred = model(torch.from_numpy(np.copy(chord_vectors[s])))
-            similar = chord_vectors.similar_by_vector(pred.numpy(), topn=6)
-            chords, _ = zip(*similar)
-            print(chords)
-            if not s:
+
+            if chord_sequence_str == "":
                 break
+
+            chord_sequence = torch.stack(
+                [
+                    torch.from_numpy(np.copy(chord_vectors[c]))
+                    for c in chord_sequence_str.split()
+                ]
+            )
+            pred = model(chord_sequence)
+            similar = chord_vectors.similar_by_vector(pred[-1].numpy(), topn=6)
+            chords, _ = zip(*similar)
+            print(chord_sequence)
+            print(pred)
+            print(chords)
 
 
 if __name__ == "__main__":
-    main()
+    test()
